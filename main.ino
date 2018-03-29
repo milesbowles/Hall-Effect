70f8b2e5053b0e6d3752339903b82fb351f67b92
Miles <Milesvbowles@gmail.com>
2018-03-28 23:58:29 -0700

    Minor refactoring

--- main.ino
+++ main.ino
@@ -1,3 +1,4 @@
+#define lightPin 4
 
 // ---------------------------------------------------------------
 // Variables
@@ -8,17 +9,25 @@
 // Setup
 // ---------------------------------------------------------------
 
-void setup()
-{
-
-    Serial.begin(9600);
-}
+ void setup()
+ {
+   Serial.begin(9600);
+   attachInterrupt(0, magnet_detect, RISING);
+   digitalWrite(lightPin, LOW);
+ }1
 
 // ---------------------------------------------------------------
 // Main loop
 // ---------------------------------------------------------------
 
-void loop()
-{
+ 
+ void loop()
+ {
 
-}
+ }
+
+  void magnet_detect() //Triggered whenever a magnet/interrupt is detected by the arduino
+ {
+   Serial.println("Magnet Detected");
+   digitalWrite(lightPin, HIGH);
+ }
